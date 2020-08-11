4873504908f2c1f377fd8eac72e9bd6f9cb8bb9e
RockingPanda <www.ahmadiftikhar@gmail.com>
2020-08-11 16:47:26 +0500

    Added Scenario browser

---
+++ DeviceSimulatorData/Assets/FetchSesorsofscenario.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FetchSesorsofscenario : MonoBehaviour
+{
+    public Dropdown dd;
+
+    public string GeneralscenarioPath;
+    public string CurrentscenarioPath;
+    public ReadFileSpawnOnMap rfsom;
+    public string Scenariosensor = "";
+   
+
+    public enum Scenarios 
+    { 
+        Fire,Flood,Shooter,Transit   
+    }
+
+    public GameObject Firepannel;
+    public GameObject Floodpannel;
+    public GameObject Shooterpannel;
+    public GameObject Transitpannel;
+
+    public void Selectchange() 
+    {
+        switch (dd.value)
+        {
+            case (int) Scenarios.Fire :
+                Firepannel.SetActive(true);
+                Floodpannel.SetActive(false);
+                Shooterpannel.SetActive(false);
+                Transitpannel.SetActive(false);
+                break;
+            case (int)Scenarios.Flood:
+                Firepannel.SetActive(false);
+                Floodpannel.SetActive(true);
+                Shooterpannel.SetActive(false);
+                Transitpannel.SetActive(false);
+
+                break;
+            case (int)Scenarios.Shooter:
+                Firepannel.SetActive(false);
+                Floodpannel.SetActive(false);
+                Shooterpannel.SetActive(true);
+                Transitpannel.SetActive(false);
+
+                break;
+            case (int)Scenarios.Transit:
+                Firepannel.SetActive(false);
+                Floodpannel.SetActive(false);
+                Shooterpannel.SetActive(false);
+                Transitpannel.SetActive(true);
+                 break;
+        }
+    
+    }
+
+    public void EnableReadFileInfo() 
+    {
+        rfsom.SimulateDataFilePath =CurrentscenarioPath;
+        rfsom.gameObject.SetActive(true);
+
+
+    }
+
+
+}
---
+++ DeviceSimulatorData/Assets/FileName.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FileName : MonoBehaviour
+{
+	public string[] FileNames;
+	public int index=-1;
+	public Button Procceed;
+
+	public FetchSesorsofscenario fsos;
+	public void SetFileindex1(bool val)
+	{
+		if (val)
+		{
+			index = 0;
+			Debug.Log("Value changed" + val);
+		}
+		else
+		{
+			index = -1;
+		}
+	}
+	public void SetFileindex2(bool val)
+	{
+		if (val)
+		{
+			index = 1;
+			Debug.Log("Value changed" + val);
+		}
+		else
+		{
+			index = -1;
+		}
+	}
+	public void SetFileindex3(bool val)
+	{
+		if (val)
+		{
+			index = 2;
+			Debug.Log("Value changed" + val);
+		}
+		else
+		{
+			index = -1;
+		}
+	}
+	public void SetFileindex4(bool val)
+	{
+		if (val)
+		{
+			index = 3;
+			Debug.Log("Value changed" + val);
+		}
+		else
+		{
+			index = -1;
+		}
+	}
+	public void SetFileindex5(bool val)
+	{
+		if (val)
+		{
+			index = 4;
+			Debug.Log("Value changed" + val);
+		}
+		else
+		{
+			index = -1;
+		}
+	}
+	public void SetFileindex6(bool val)
+	{
+		if (val)
+		{
+			index = 5;
+			Debug.Log("Value changed" + val);
+		}
+		else
+		{
+			index = -1;
+		}
+	}
+	public void SetFileindex7(bool val)
+	{
+		if (val)
+		{
+			index = 6;
+			Debug.Log("Value changed" + val);
+		}
+		else
+		{
+			index = -1;
+		}
+	}
+	public void SetFileindex8(bool val)
+	{
+		if (val)
+		{
+			index = 7;
+			Debug.Log("Value changed" + val);
+		}
+		else 
+		{
+			index = -1;
+		}
+	}
+	public void SetFileindex9(bool val)
+	{
+		if (val)
+		{
+			index = 8;
+			Debug.Log("Value changed" + val);
+		}
+		else
+		{
+			index = -1;
+		}
+	}
+	private void Update()
+	{
+		if (index == -1) 
+		{
+			Procceed.interactable = false;
+		}
+		if (index != -1)
+		{
+			Procceed.interactable = true;
+		}
+	}
+
+	public void ProcceedWIthName()
+	{
+		if (index != -1)
+		{
+			fsos.Scenariosensor = FileNames[index];
+			fsos.CurrentscenarioPath = fsos.GeneralscenarioPath + fsos.Scenariosensor;
+			fsos.EnableReadFileInfo();
+		}
+	}
+}
